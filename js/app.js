7f8ffff67be2f978bdd46e09c5d7db276c27d1c3
mandyreal <normandy.real@gmail.com>
2015-01-22 10:59:46 +0800

    refined random speed generation and include collision detection logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // globale variables used by game environment
-var rowHeight = 83;
-var columnWidth = 101;
+// this variable controls how fast the bugs move, 
+// can be set from 200 onwards with 200 as normal speed
+var bugSpeed = 200;
 
 // Enemies our player must avoid
 var Enemy = function(x,y) {
@@ -12,7 +13,6 @@ var Enemy = function(x,y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    this.speed = [3,1017,250,4,6,3];
 }
 
 // Update the enemy's position, required method for game
@@ -21,10 +21,21 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed[Math.round(Math.random()*5)]*dt;
-    //this.x += 50 * this.speed * dt;
+    
+    // Generate random speeds for each bug for every page refresh
+    this.x += Math.round((Math.random()* 100) + bugSpeed) * dt;
+
+    // Collission detection logic. Reset player to starting position
+    // when any of the bugs collides with the player
+    if ((this.x - player.x <  50 && this.y - player.y < 50) && 
+        (this.x - player.x > -50 && this.y - player.y > -50)) {
+        player.reset();
+    }
+
+    // Check if bug location has reached the right end, 
+    // then reset bug's location to random starting point
     if (this.x > 400) {
-       this.x -= 400;
+       this.x = -(Math.round(Math.random()*500));
     }
     
 }
@@ -48,8 +59,8 @@ Player.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x*dt;
-    this.y*dt;
+    this.x * dt;
+    this.y * dt;
 }
 
 // Draw the player on the screen, required method for game
@@ -59,6 +70,8 @@ Player.prototype.render = function() {
 
 // Reset player position to starting point 
 Player.prototype.reset = function() {
+    // Default starting point for player, 
+    // prefer to start always in middle of screen
     this.x = 205;
     this.y = 308;
 }
@@ -66,10 +79,8 @@ Player.prototype.reset = function() {
 Player.prototype.handleInput = function(keynum) {
     switch(keynum)  {
         case 'up':
-//           console.log(this.y);
             if(this.y > 38) {
                 this.y -= 90;
-
             }
             // this means player hits water, reset to initial position
             else {
@@ -103,9 +114,10 @@ Player.prototype.handleInput = function(keynum) {
 
 var player = new Player(205,308);
 
-var enemy1 = new Enemy(5,55);
-var enemy2 = new Enemy(55,145);
-var enemy3 = new Enemy(105,225);
+// assign randon starting x coordinate for each bug to add some randomness
+var enemy1 = new Enemy(Math.round(Math.random()*105),55);
+var enemy2 = new Enemy(Math.round(Math.random()*105),145);
+var enemy3 = new Enemy(Math.round(Math.random()*105),225);
 
 allEnemies = [enemy1, enemy2, enemy3];
 
